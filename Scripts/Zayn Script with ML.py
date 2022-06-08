7e7075dc858ced66e8c57594de93fc461a888630
Zayn Roohi <zayn.roohi@dot.gov>
2022-06-08 17:07:15 -0400

    Initial Neural Net Setup
    Creates a neural net and then data regression, currently produces average results (R2 = 0.9). Need to add in data classification and then further run for loops to find the best neural net params

--- Scripts/Zayn Script with ML.py
+++ Scripts/Zayn Script with ML.py
@@ -1,13 +1,15 @@
+from gc import callbacks
 import pandas as pd
 import numpy as np
 import tensorflow as tf
 import tensorflow_addons as tfa
+import pathlib, os
 
 def PrepareData(DataInPath):
-    print("\nImporting Data...")
     RawData = pd.read_csv(DataInPath)
-    print("Filtering & Processing Data...")
     LiftoffData = RawData[RawData['LIFTOFF'] == 1]
+    #Change to make: Instead of dropping data, import only the columns we need
+    #This will be a bit difficult as sometimes columns have the same name
     MLData = LiftoffData.drop(['TIME_OFFSET', 'FLIGHT_ID', 'FLIGHT_ID.1', 'STATED_SEGMENT_START_OF_TAKEOFF', 'APT_AIRCRAFT_RUNWAY_STAGE',
                             'LIFTOFF', 'TIME_ON_GROUND_BEFORE_LIFTOFF_(SECONDS)', 'P64: Duration of Taxi Out (Minutes)',
                             'DURATION','SPEED_SOUND_START_EVENT', 'AFE_ALT', 'TAS_START_EVENT', 'P64: True Airspeed at Liftoff (knots)',
@@ -25,20 +27,17 @@ def PrepareData(DataInPath):
     MLData.rename(columns = {'ACTYPE.1' : 'ACTYPE'}, inplace = True)
     MLData.rename(columns = {'APT_AIRCRAFT_RUNWAY_STAGE.1' : 'APT_AIRCRAFT_RUNWAY_STAGE'}, inplace = True)
     
+    #Change: Split the APT_AIRCRAFT_RUNWAY_STAGE column into 4 (and possible drop STAGE?)
     MLData = MLData.dropna()
     MLData = MLData.sample(frac = 1, replace = False)
-
-    #Need to figure out a way to not drop these string columns, but for now will keep it as this
+    #Classifications don't do anything right now, will add them in next
+    ClassificationData = MLData[['ACTYPE', 'APT_AIRCRAFT_RUNWAY_STAGE']]
     MLData = MLData.drop(['ACTYPE', 'APT_AIRCRAFT_RUNWAY_STAGE'], axis = 1)
 
-    print("Preparing Data for ML...")
-
     trn, val, test = np.split(MLData, [int(0.7*len(MLData)), int(0.85*len(MLData))], axis = 0)
-
-
     trn_independent = trn.drop('N1', axis = 1)
-    val_independent = trn.drop('N1', axis = 1)
-    test_independent = trn.drop('N1', axis = 1)
+    val_independent = val.drop('N1', axis = 1)
+    test_independent = test.drop('N1', axis = 1)
     trn_dependent = trn.N1
     val_dependent = val.N1
     test_dependent = test.N1
@@ -49,36 +48,70 @@ def PrepareData(DataInPath):
 
     return FinalDataset
 
-def DefineNNArchitecture(Dataset):
-    print("Creating ML Model...\n")
+def DefineAndTrainNN(Dataset, MainPath):
+    #Create the model
+    #Note - need to actually figure out the best settings here
+    #Changes should be number of layers, numer of neurons, activation function
+    #also need to look at learning rate, possibly add a kernal regularizer
+    #Later on the number of epochs
+    #Finally, decide if adam is really the best optimizer for this type of problem (I think it is?)
     normlayer = tf.keras.layers.Normalization()
-    normlayer.adapt(Dataset['trn']['i'])
+    normlayer.adapt(Dataset['trn']['i'].to_numpy())
 
     model = tf.keras.Sequential([
-        normlayer,
-        tf.keras.layers.Dense(8, activation = 'tanh'),
-        tf.keras.layers.Dense(1, activation = 'linear')])
+                normlayer,
+                tf.keras.layers.Dense(units = 8, activation = 'relu'),
+                tf.keras.layers.Dense(units = 16, activation = 'relu'),
+                tf.keras.layers.Dense(units = 32, activation = 'relu'),
+                tf.keras.layers.Dense(units = 1, activation = 'linear')])
 
     model.compile(
                 optimizer = tf.keras.optimizers.Adam(learning_rate = 0.1),
                 loss = 'mse',
                 metrics = [tf.metrics.MeanSquaredError(), tf.keras.metrics.MeanAbsoluteError(), tfa.metrics.r_square.RSquare()])
 
-    return model
 
-#def TrainModel():
-    #Train the model - save the best fit model
+    #Train the model now
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+                filepath = os.path.join(MainPath, 'Out/Checkpoints/checkpoint'),
+                save_weights_only = True,
+                save_best_only = True,
+                monitor = 'loss',
+                mode = 'min')
+    learning_callback = tf.keras.callbacks.ReduceLROnPlateau(
+                monitor = 'loss',
+                factor = 0.1,
+                patience = 5,
+                verbose = 1,
+                mode = 'min',
+                min_delta = 0.0001,
+                cooldown = 25,
+                min_lr = 0.000000000000000000000001)
+
+    history = model.fit(
+                Dataset['trn']['i'],
+                Dataset['trn']['d'],
+                batch_size = 400,
+                epochs = 500,
+                validation_data = (Dataset['val']['i'], Dataset['val']['d']),
+                callbacks = [checkpoint_callback, learning_callback])
+
+    model.load_weights(os.path.join(MainPath, 'Out/Checkpoints/checkpoint'))
+    model.save(os.path.join(MainPath, 'Out/Models/model'))
+    print('\n-----------Best Training Data:-----------\n')
+    print(model.evaluate(Dataset['trn']['i'], Dataset['trn']['d']))
+
 
 #def PlotOutputs():
     #Plot the output of the final best-fit model to see how good of a model it was
+    #Don't necessarily need to do this as the R^2 does the same thing, but visualizations are always nice
 
 #Actually run the program
-#MainPath = pathlib.Path(__file__).parent.parent.resolve()
-#OutPath = os.path.join(MainPath, "Out")
+MainPath = pathlib.Path(__file__).parent.parent.resolve()
 DataPath = 'C:/Users/Zayn.Roohi/Documents/OASIS/TestDataLarge.csv' #takeoff_distance_A320_A330_A340.csv'
 
 Dataset = PrepareData(DataPath)
-model = DefineNNArchitecture(Dataset)
+DefineAndTrainNN(Dataset, MainPath)
 
-#Simple data structures: https://www.tensorflow.org/tutorials/keras/regression
-#Create data structures: https://www.tensorflow.org/tutorials/structured_data/preprocessing_layers
+#Simple  string data structures: https://www.tensorflow.org/tutorials/keras/regression
+#Complex string data structures: https://www.tensorflow.org/tutorials/structured_data/preprocessing_layers
